65f2a3101384b77edcb613f5d954a66be9494200
eljosephavila123 <josdavil@espol.edu.ec>
2020-03-23 18:29:39 -0500

    Schema

--- backend/src/routes /patients.js
+++ backend/src/routes /patients.js
@@ -1,7 +1,8 @@
-const Patient =require('./mo');
+
 const { Router }=require('express');
 
 const router= Router();
+const Patient =require('../models/Patient');
 
 const faker=require('faker');
 router.get('/api/patient',async(req,res)=>{
@@ -16,16 +17,17 @@ router.get('/api/patient/create', async (req,res)=>{
         await Patient.create({
             firstName:faker.name.firstName(),
             lastName:faker.name.lastName(),
-            age:faker.date(),
-            dni:faker.date(),
-            email:faker.date(),
-            emerPhone:faker.date(),
-            contcPhone:faker.date()
+            age:faker.name.firstName(),
+            dni:faker.name.firstName(),
+            email:faker.name.firstName(),
+            emerPhone:faker.name.firstName(),
+            contcPhone:faker.name.firstName(),
         });
 
-    }
+    };
 
     res.json({message:'5 pacientes creados'});
 
 });
 module.exports=router;
+
